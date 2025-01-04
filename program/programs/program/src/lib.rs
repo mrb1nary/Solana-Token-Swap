a1381381c32f02496e559aca5bd9b5c95c69f6f3
mrb1nary <anubhab.techie@gmail.com>
2025-01-04 15:06:59 +0530

    Make offer function

--- program/programs/program/src/lib.rs
+++ program/programs/program/src/lib.rs
@@ -12,10 +12,16 @@ pub use state::*;
 declare_id!("DawEAogFrCwK3R7H4Mp9NMoUo1mbYQMR3zT6d5vLRaoh");
 
 #[program]
-pub mod program {
+pub mod swap {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn make_offer(
+        ctx: Context<MakeOffer>,
+        id: u64,
+        token_a_offered_amount: u64,
+        token_b_wanted_amount: u64,
+    ) -> Result<()> {
+        instructions::make_offer::send_offered_tokens_to_vault(&ctx, token_a_offered_amount)?;
+        instructions::make_offer::save_offer(ctx, id, token_b_wanted_amount)
     }
 }
