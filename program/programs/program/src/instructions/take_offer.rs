bbfa65ff101938f8bb9f2ee586a89d3afd28d8c9
mrb1nary <anubhab.techie@gmail.com>
2025-01-05 20:24:21 +0530

    Added take offer struct

---
+++ program/programs/program/src/instructions/take_offer.rs
@@ -0,0 +1,70 @@
+use anchor_lang::prelude::*;
+
+use anchor_spl::{
+    associated_token::AssociatedToken, token::Token, token_interface::{
+        close_account, transfer_checked, CloseAccount, Mint, TokenAccount, TokenInterface,
+        TransferChecked,
+    }
+};
+
+use crate::Offer;
+
+use super::transfer_tokens_internal;
+
+#[derive(Accounts)]
+pub struct TakeOffer<'info> {
+    #[account(mut)]
+    pub taker: Signer<'info>,
+
+    #[account(mut)]
+    pub maker: SystemAccount<'info>,
+
+    pub token_mint_a: InterfaceAccount<'info, Mint>,
+
+    pub token_mint_b: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        init_if_needed,
+        payer=taker,
+        associated_token::mint = token_mint_a,
+        associated_token::authority = taker,
+        associated_token::token_program = token_program,
+    )]
+    pub taker_token_account_a: Box<InterfaceAccount<'info, TokenAccount>>,
+
+    #[account(
+        mut,
+        associated_token::mint = token_mint_b,
+        associated_token::authority = taker,
+        associated_token::token_program = token_program,
+    )]
+    pub taker_token_account_b: Box<InterfaceAccount<'info, TokenAccount>>,
+
+    #[account(
+        init_if_needed,
+        payer = taker,
+        associated_token::mint = token_mint_b,
+        associated_token::authority = maker,
+        associated_token::token_program = token_program,
+    )]
+    pub maker_token_account_b: Box<InterfaceAccount<'info, TokenAccount>>,
+
+    #[account(
+        mut,
+        close=maker,
+        has_one=maker,
+        has_one=token_mint_a,
+        has_one=token_mint_b,
+        seeds = [b"offer", maker.key().as_ref(), offer.id.to_le_bytes().as_ref()],
+        bump = offer.bump
+    )]
+    offer: Account<'info, Offer>,
+
+    #[account(
+        mut,
+        associated_token::mint = token_mint_a,
+        associated_token::authority = offer,
+        associated_token::token_program = token_program,
+    )]
+    vault: InterfaceAccount<'info, TokenAccount>,
+}
